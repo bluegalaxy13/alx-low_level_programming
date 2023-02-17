d3b2b250aeec4ebe2d5bc62387d060a2bc80d66e
Ricardo <ricardosassofficial@gmail.com>
2023-02-17 11:35:47 -0800

    Fixed changes

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -7,6 +7,7 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char low;
@@ -14,7 +15,9 @@ int main(void)
 	for (low = 'z'; low >= 'a'; low--)
 	{
 		putchar(low);
-	}	
+	}
 	putchar('\n');
+
 	return (0);
 }
+
