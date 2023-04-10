07dd13b765ba70ad8106d9d42cfa8cdc2e4be972
Ricardo Sass <ricardosassofficial@gmail.com>
2023-04-10 20:57:57 +0200

    Fixing betty again

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 /**
- * flip_bits - A function that gets number of bits needed to flip
- * @n: How many bit flips are needed to equal m for n
- * @m: The number to set other equal
+ * flip_bits - gets number of bits needed to flip
+ * @n: how many bit flips are needed to equal m for n
+ * @m: number to set other equal
  * Return: The number of fliped bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
